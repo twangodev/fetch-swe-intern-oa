162e2bb587c82b5ed512be0dd9c2bf52f61ede33
James Ding <jamesding365@gmail.com>
2025-02-27 17:35:11 -0600

    Add top app bar

--- app/src/main/java/dev/twango/fetch/main/MainScreen.kt
+++ app/src/main/java/dev/twango/fetch/main/MainScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -38,9 +39,14 @@ fun FetchTakeHomeApp() {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FetchTakeHomeTopBar() {
-
+    TopAppBar(
+        title = {
+            Text(text = "Fetch Take Home")
+        }
+    )
 }
 
 @Composable
